717562d4f36b7ebe9e3827efd4e1edf50d1e3bd3
hellcattc <slhtte345@gmail.com>
2022-11-04 18:58:35 +0300

    Fixed scrollbar

--- rainScript.js
+++ rainScript.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById("myCanvas");
-canvas.style.width = this.window.innerWidth;
-canvas.style.height = this.window.innerHeight;
+canvas.width = this.window.innerWidth;
+canvas.height = this.window.innerHeight;
 var ctx = canvas.getContext("2d")
 
 var randDistr;
